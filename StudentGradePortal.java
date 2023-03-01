3553463a2086e9f1b89e4a50375d31cd617a2928
AliakbarETH <aliakbar.bcss19@iba-suk.edu.pk>
2023-03-01 10:24:04 +0500

    Added code schema below

--- StudentGradePortal.java
+++ StudentGradePortal.java
@@ -1,7 +1,54 @@
 public class StudentGradePortal {
 
     public static void main(String args[]) {
-        
+
     }
     
 }
+
+/*
+ * Things to do:
+ * 
+ *  Make a Student Grade Portal
+ * 
+ *  Variables
+ * 
+ *  String studentName;
+ *  String address;
+ *  String studentCMSID;
+ *  int noOfLabCourses ;
+ *  int noOfTheoryCourses ;
+ *  int labMarks [noOfLabCourses], theoryMarks [noOfTheoryCourses] ;
+ *  String nameOfLabCourses[noOfLabCourses], nameOfTheoryCourses[noOfTheoryCourses]
+ *  String lab = nameOfLabCourses[noOfLabCourses], theory = nameOfTheoryCourses[noOfTheoryCourses]
+ *  String studentCoursesArray [nameOfLabCourses[] + nameOfTheoryCourses] ;
+ * 
+ *  Constructors
+ *  //Default
+ *  // Parameterized
+ *  
+ *  Methods
+ * 
+ *  // See all students record ( Pass + Fail)
+ *  // Add new Student
+ *  // Update Student Record
+ *  // Delete Student Record
+ *  // formaula to calculate gpa
+ *  // calculate if a person is pass or fail
+ * 
+ *  Classes
+ * 
+ *  main class => StudentGradePortal
+ *  supporting class => Student - to hold all the information regarding student
+ * 
+ *  Objects
+ * 
+ *  Student object 
+ * 
+ *  
+ * 
+ *  
+ * 
+ *  
+ * 
+ */
